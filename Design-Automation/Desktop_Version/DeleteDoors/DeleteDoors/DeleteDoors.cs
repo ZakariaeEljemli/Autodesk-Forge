91a6aba1cbfe614fce32eb32a607a34aa15ea388
ELJEMLI <zakariaeeljemli1996@gmail.com>
2021-04-18 13:01:30 +0200

    add the Desktop Version of the DeleteDoors Addin for Revit

---
+++ Design-Automation/Desktop_Version/DeleteDoors/DeleteDoors/DeleteDoors.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace DeleteDoors
+{
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+
+    public class DeleteDoorsApp : IExternalCommand
+    {
+
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var app = commandData.Application.Application;
+            var doc = commandData.Application.ActiveUIDocument?.Document;
+
+            DeleteAllDoors(app, doc);
+            return Result.Succeeded;
+        }
+
+        public static void DeleteElement(Document document, Element element)
+        {
+            // Delete a selected element
+            ICollection<Autodesk.Revit.DB.ElementId> deletedIdSet = document.Delete(element.Id);
+
+
+            if (0 == deletedIdSet.Count)
+            {
+                throw new Exception("Deleting the selected element in Revit failed.");
+            }
+
+
+            String prompt = "The selected element has been removed and ";
+            prompt += deletedIdSet.Count - 1;
+            prompt += " more dependent elements have also been removed.";
+
+
+            // Give the user some information
+            TaskDialog.Show("Revit", prompt);
+
+
+        }
+
+        public static void DeleteAllDoors(Application rvtApp, Document doc)
+        {
+            if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
+
+
+            if (doc == null) throw new InvalidOperationException("Could not open document.");
+
+
+            IList<Element> listOfDoors = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Doors).WhereElementIsNotElementType().ToElements();
+            int taille = listOfDoors.Count;
+
+
+            if (taille == 0)
+            {
+
+
+                TaskDialog.Show("Dialog", "There isn't any door in the project");
+            }
+
+            else
+            {
+                using (Transaction tr = new Transaction(doc))
+                {
+
+
+                    tr.Start("do");
+                    for (int i = 0; i < taille; i++)
+                    {
+
+                        DeleteElement(doc, listOfDoors[i]);
+                    }
+
+
+                    tr.Commit();
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Design-Automation/Desktop_Version/DeleteDoors/ReadMe.md.txt
@@ -0,0 +1,37 @@
+# DeleteDoors Sample (Desktop Version)
+
+[![.net](https://img.shields.io/badge/.net-4.7|4.8-green.svg)](http://www.microsoft.com/en-us/download/details.aspx?id=30653)
+[![visual studio](https://img.shields.io/badge/Visual%20Studio-2017|2019-green.svg)](https://www.visualstudio.com/)
+[![revit](https://img.shields.io/badge/revit-2018|2019|2020|2021-red.svg)](https://www.autodesk.com/products/revit/overview/)
+
+## Description
+
+DeleteDoors is a command that deletes all doors in the current file.
+
+## Dependencies
+
+This project was built in Visual Studio 2019. Download it [here](https://www.visualstudio.com/).
+
+This sample references Revit 2021's `RevitAPI.dll` and `RevitAPIUI.dll`.
+
+## Building DeleteDoors.sln
+
+Find `RevitAPI.dll` and `RevitAPIUI.dll` in your Revit 2021 install location and note its location. 
+
+Clone this repository and open `DeleteDoors.sln` in Visual Studio.  
+
+In the DeleteDoors C# project, repair the references  `RevitAPI` and `RevitAPIUI`.  You can do this by removing and re-adding the references, or by opening the `DeleteDoors.csproj` for edit and manually updating the reference paths.
+
+Build `DeleteWalls.sln` in `Release` or `Debug` configuration.
+
+__Note: Although the project will be always migrated to latest release of Revit, it should be also working with previous releases (after re-built by the corresponding references & packages).__
+
+## Using DeleteDoors
+
+Find `DeleteDoors.addin` and place in the Revit Addins folder (`C:\ProgramData\Autodesk\Revit\Addins\201*\` for Windows 7, 8, and 10).
+
+Edit the .addin file and edit the `<Assembly>` value to point to the location of `Autodesk.DeleteDoors.Command.dll`.
+
+Start up Revit and open a Revit file.
+
+Under the Add-Ins ribbon tab, click the **External Tools** drop-down, then click **DeleteDoors**.
